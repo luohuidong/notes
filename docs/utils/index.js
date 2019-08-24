259645a47ce5be787079c8f7a8566067221d1dbe
luohuidong <luohuidong01@126.com>
2019-08-24 13:39:59 +0800

    添加注释

--- docs/utils/index.js
+++ docs/utils/index.js
@@ -1,3 +1,9 @@
+/**
+ * 获取文档的目录
+ * 如 /mnt/d/project/notes/docs/frontend-basic/typescript
+ * 处理后将得到 /frontend-basic/typescript/
+ * @param {string} dirName 文档所在文件目录路径
+ */
 function getDocsDirectory(dirName) {
   return dirName.split('/docs')[1] + '/'
 }
