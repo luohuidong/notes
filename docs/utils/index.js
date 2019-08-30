baf62b61c8fff59a373364a27e22983e923a6c89
luohuidong <luohuidong01@126.com>
2019-08-31 00:53:10 +0800

    修改获取文档所在路径的逻辑

--- docs/utils/index.js
+++ docs/utils/index.js
@@ -1,11 +1,15 @@
+const path = require('path')
+
 /**
- * 获取文档的目录
+ * 获取文档的目录，文档对应路径的规则请看 vuepress 文档
  * 如 /mnt/d/project/notes/docs/frontend-basic/typescript
  * 处理后将得到 /frontend-basic/typescript/
  * @param {string} dirName 文档所在文件目录路径
  */
 function getDocsDirectory(dirName) {
-  return dirName.split('/docs')[1] + '/'
+  const paths = dirName.split(path.sep)
+  paths.indexOf('docs')
+  return '/' + paths.slice(paths.indexOf('docs') + 1).join('/') + '/'
 }
 
 module.exports = {
