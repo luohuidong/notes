4d4315340bc3863bf9ba940b04fbb43efce6e712
luohuidong <luohuidong01@126.com>
2019-09-04 00:59:38 +0800

    新增 electron 参考资料

--- docs/advance/electron/index.js
+++ docs/advance/electron/index.js
@@ -2,7 +2,7 @@ const { getDocsDirectory } = require('../../utils')
 
 const path = getDocsDirectory(__dirname)
 const title = 'Electron'
-const firstPost = 'start'
+const firstPost = 'material'
 
 const navItem = {
   text: title,
@@ -15,6 +15,7 @@ const sidebar = {
       title,
       children: [
         firstPost,
+        'start',
         'electron-builder',
       ]
     }
