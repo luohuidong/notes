166ba59d38759be17933347e7146ea391a76240c
luohuidong <luohuidong01@126.com>
2020-01-07 08:46:40 +0800

    Add

--- docs/advance/index.js
+++ docs/advance/index.js
@@ -6,8 +6,12 @@ const electronConfig = require('./electron')
 const gitConfig = require('./git')
 const reactConfig = require('./react')
 const reactNativeConfig = require('./react-native')
+const softwareDevelopment = require('./software-development')
 
-const docs = [webpackConfig, babelConfig, electronConfig, gitConfig, reactConfig, reactNativeConfig]
+const docs = [
+  webpackConfig, babelConfig, electronConfig, gitConfig, reactConfig,
+  reactNativeConfig, softwareDevelopment
+]
 const nav = {
   text: '进阶',
   items: []
---
+++ docs/advance/software-development/index.js
@@ -0,0 +1,24 @@
+const { getDocsDirectory } = require('../../utils')
+
+const path = getDocsDirectory(__dirname)
+
+const navItem = {
+  text: '软件开发',
+  link: `${path}2020-1-7`
+}
+
+const sidebar = {
+  [path]: [
+    {
+      title: '敏捷开发',
+      children: [
+        '2020-1-7',
+      ]
+    }
+  ]
+}
+
+module.exports = {
+  navItem,
+  sidebar
+}
