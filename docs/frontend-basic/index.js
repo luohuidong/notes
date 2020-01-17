94b2e3d3628a886b9a91d0a016721ec316831415
luohuidong <luohuidong01@126.com>
2020-01-17 21:57:03 +0800

    Update

--- docs/frontend-basic/index.js
+++ docs/frontend-basic/index.js
@@ -1,18 +1,46 @@
-const { generateNavAndSidebarConfig } = require('../utils')
-
 const material = require('./material')
 const cssConfig = require('./css')
 const es5Config = require('./es5')
 const typescriptConfig = require('./typescript')
 const npmConfig = require('./npm')
 
-const docs = [ material, cssConfig, es5Config, typescriptConfig, npmConfig ]
+const reactConfig = require('./react')
+const reactNativeConfig = require('./react-native')
+
+const webpackConfig = require('../frontend-basic/webpack')
+const babelConfig = require('./babel')
+const electronConfig = require('../frontend-basic/electron')
+
 const nav = {
   text: '前端',
-  items: []
+  items: [
+    {
+      text: '基础',
+      items: [
+        material.navItem, cssConfig.navItem, es5Config.navItem, typescriptConfig.navItem,
+        npmConfig.navItem
+      ]
+    },
+    {
+      text: '工程化',
+      items: [webpackConfig.navItem, babelConfig.navItem]
+    },
+    {
+      text: 'React 全家桶',
+      items: [reactConfig.navItem, reactNativeConfig.navItem]
+    },
+    {
+      text: '跨平台',
+      items: [electronConfig.navItem]
+    }
+  ]
 }
-const sidebar = {}
-generateNavAndSidebarConfig(docs, nav, sidebar)
+const sidebar = Object.assign({},
+  material.sidebar, cssConfig.sidebar, es5Config.sidebar, typescriptConfig.sidebar, npmConfig.sidebar,
+  webpackConfig.sidebar, babelConfig.sidebar,
+  reactConfig.sidebar, reactNativeConfig.sidebar,
+  electronConfig.sidebar
+)
 
 module.exports = {
   nav,
